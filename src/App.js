cdf3f5fe82731b74e02750c8c7d98f76ed26d864
Robin Hutmacher <robin.hutmacher@gmail.com>
2021-01-30 12:06:59 +0100

    Add recipe generating code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Recipes from "./recipes";
+
+const Step = ({ step }) => {
+  const [title, description] = step;
+  return (
+    <div className="py-3 border-b-2">
+      <h1 className="font-semibold text-xl underline">{title}</h1>
+      <p className="pl-4 pt-2">{description}</p>
+    </div>
+  );
+};
+
+const TartineRecipe = () => {
+  const steps = Object.entries(Recipes["Tartine"]["steps"]);
+  return (
+    <div>
+      {steps.map((step) => (
+        <Step key={step[0]} step={step} />
+      ))}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-yellow-50 font-serif">
+      <header className="bg-yellow-400">
+        <nav className="w-7/12 mx-auto">
+          <ul className="flex flex-row space-x-6 font-light text-3xl">
+            <li>Bread</li>
+            <li>Pizza</li>
+          </ul>
+        </nav>
       </header>
+      <div className="w-7/12 mx-auto max-w-2xl mt-8">
+        <TartineRecipe />
+      </div>
     </div>
   );
 }
---
+++ src/recipes.js
@@ -0,0 +1,16 @@
+const Recipes = {
+  Tartine: {
+    steps: {
+      "Day 1, Morning":
+        "Take the starter and feed it as described above (10g Starter, 50g Rye flour, 50g water). Let it rest at room temperature for the day.",
+      "Day 1, Evening":
+        "Mixing the main dough: The activated starter (110g), 500g flour (there you can vary a lot, I mainly use 60-70% T550 wheat flour and the rest either whole wheat, whole spelt or rye. Though rye doughs turn very sticky, so it's probably easier to try this after some initial experiments), 375g water (probably easier to start the first breads with something like 330g), 12g salt. Mix until the dough is almost smooth. Let it rest for 10-15 minutes, mix again unitl smooth. Now the dough rests for something like 2h. During that period the dough is strech and folded every 30 minutes, if you find the time. After the 2h passed, it's put in the fridge. This stage is called the bulk fermentation.",
+      "Day 2, Morning":
+        "Shaping the dough. Getting the dough out of the fridge and on the counter. Pre-shape, let it rest for 10-15 minutes, and final shaping. That's hard to explain, I'll add some links to videos. Or there is also a good description in Tartine Bread. Place the shaped loaf in a proofing basket or a line sieve as alternative, but it in a plastic bag (so it doesn't dry out) and but it back in the fridge.",
+      "Day 2, Evening":
+        "I normally bake in a dutch oven. Pre-heating the oven with the dutch-oven inside upto 250°C for 30 minutes. During that time you can let the loaf rest at room temperature, too. Bake the bread at 250°C for 30 minutes with the dutch-oven lid on, and another 15 minutes without the lid.",
+    },
+  },
+};
+
+export default Recipes;
